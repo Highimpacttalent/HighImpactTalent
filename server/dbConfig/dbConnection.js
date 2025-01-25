cb679f1bef498163b922d5650cd65294518d2e8a
Highimpacttalent <developerhighimpact@gmail.com>
2025-01-25 18:40:04 +0530

    Update dbConnection.js

--- server/dbConfig/dbConnection.js
+++ server/dbConfig/dbConnection.js
@@ -1,7 +1,9 @@
 import mongoose from "mongoose";
 const dbConnection = async () => {
     try {
-        const dbConnection = await mongoose.connect(process.env.MONGODB_URL);
+        // const dbConnection = await mongoose.connect(process.env.MONGODB_URL);
+         const dbConnection = await mongoose.connect('mongodb+srv://developerhighimpact:j5dnAEhz8FZibFrb@cluster0.yjt2q.mongodb.net/jobportal?retryWrites=true&w=majority&appName=Cluster0',
+  { useNewUrlParser: true, useUnifiedTopology: true });
 
         console.log("Database Connected");
     } catch (error) {
