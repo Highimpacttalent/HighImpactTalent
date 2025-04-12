ef3ea15aaae20438089bdd601eab31bac81f2460
Anuradha-Udatha <udathaanuradha2313@gmail.com>
2025-04-12 21:22:08 +0530

    fixed pagination

--- server/controllers/jobController.js
+++ server/controllers/jobController.js
@@ -335,7 +335,7 @@ export const getJobPosts = async (req, res, next) => {
 
     // Handle pagination
     const page = Number(req.query.page) || 1;
-    const limit = Number(req.query.limit) || 20;
+    const limit = Number(req.query.limit) || 10;
     const skip = (page - 1) * limit;
 
     // Get total count before applying limit
