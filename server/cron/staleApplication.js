96a8d58971270ef5cb31e9e696219a9c763e151a
Manas Singh <dev@flarelink.in>
2025-07-12 18:28:44 +0530

    Updated

--- server/cron/staleApplication.js
+++ server/cron/staleApplication.js
@@ -63,7 +63,7 @@ const runStaleApplicationReminder = async () => {
 };
 
 // Run at 6:37 PM daily
-cron.schedule('27 18 * * *', () => {
+cron.schedule('33 18 * * *', () => {
   console.log('🕒 Running daily stale application check...');
   runStaleApplicationReminder();
 });
